3cefc70132c36bd43e2d44321716675679b6a636
aditya <aditya003.ay@gmail.com>
2022-01-19 13:14:27 +0530

    Intro

---
+++ globals.js
@@ -0,0 +1,10 @@
+//Globals
+
+
+console.log(__dirname)
+
+let i = 0
+setInterval(()=>{
+    console.log(`hello ${++i} ` )
+},1000)
+
---
+++ intro.js
@@ -0,0 +1,10 @@
+const amount = 2
+
+if(amount<12){
+    console.log("less")
+}
+else {
+    console.log("more")
+    
+}
+console.log(`hello world ${amount}`)
