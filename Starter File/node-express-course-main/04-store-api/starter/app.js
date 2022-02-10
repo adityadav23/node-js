b9f4585d0d76b2b62150a1fac8d15b2ba46475ad
Aditya Yadav <adityadav023>
2022-02-10 14:04:06 +0530

    yes

--- Starter File/node-express-course-main/04-store-api/starter/app.js
+++ Starter File/node-express-course-main/04-store-api/starter/app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const productsRouter = require('./routes/products')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const port =  5000
+const connectDB = require('./db/connect')
+
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send('<h1>Store</h1> <a href =/api/v1/products> Products route </a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,()=>{
+            console.log(` Server is listening on ${port} `)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
